52abe347dfb3687ccf327c7a617977e471dbf32b
xewvvi <xewvvi@gmail.com>
2026-08-16 16:05:10 +0500

    back to sudo

--- mkboot_flash.sh
+++ mkboot_flash.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 set -e
+
 FLASH_ROOTFS=false
 for arg in "$@"; do
     if [ "$arg" = "-flash-rootfs" ]; then
         FLASH_ROOTFS=true
     fi
 done
-doas true
-doas umount -R rootfs/ 2>/dev/null || true
-cp out/arch/arm64/boot/Image.gz-dtb AIK/split_img/boot.img-kernel
-doas rm -rf AIK/ramdisk/*
-doas cp -a initramfs/src/initramfs/* AIK/ramdisk/
+
+sudo -v
+sudo umount -R rootfs/ 2>/dev/null || true
+
+sudo cp out/arch/arm64/boot/Image.gz-dtb AIK/split_img/boot.img-kernel
+
+sudo rm -rf AIK/ramdisk/*
+sudo cp -a initramfs/src/initramfs/* AIK/ramdisk/
+
 (
     cd initramfs/src/initramfs
     find . | cpio -o -H newc | gzip -9 > ../../../AIK/split_img/boot.img-ramdisk.cpio.gz
 )
+
 cd AIK/
 ./repackimg.sh
 cd ..
+
 fastboot flash boot AIK/image-new.img
+
 if [ "$FLASH_ROOTFS" = true ]; then
     echo "Flashing rootfs.img to userdata..."
-    fastboot flash userdata rootfs.img
 fi
-fastboot reboot
